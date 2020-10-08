68ab61380c32b3ec27c8f050cc430e208a17137f
Sadia Rahnuma Ashraf <sadiaashraf013@gmail.com>
2020-10-08 17:17:44 +0200

    Slight change in components

--- src/Components/navigation.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-function Navigation(props){
-    return(
-        <h1>{props.person.name} liked your status</h1>
-    )
-}
-export default Navigation;
--- src/Components/newsfeed.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-function NewsFeed(){
-    return(
-        <h1>NewsFeed!</h1>
-    )
-}
-export default NewsFeed;
